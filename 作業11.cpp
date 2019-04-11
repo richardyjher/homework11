749a4517ae65dfd1a425d5abd58996514ab28c4a
richardyjher <48574881+richardyjher@users.noreply.github.com>
2019-04-11 21:31:50 +0800

    Add files via upload

---
+++ 作業11.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdlib.h>
+ 
+
+int main(int argc, char *argv[])
+{
+	int num1,num2,num3,x; 
+	printf("請輸入三個數字進行排序：");  
+	scanf("%d%d%d",&num1,&num2,&num3); 
+	if(num1>num2)
+	{ 
+	x=num1;  
+	num1=num2; 
+	num2=x; 
+	}
+	if(num1>num3)
+	{ 
+	x=num1; 
+	num1=num3; 
+	num3=x; 
+	}
+	if(num2>num3)
+	{   
+	x=num2;   
+	num2=num3; 
+	num3=x;
+	}
+
+	printf("從小到大的排列是:%d %d %d",num1,num2,num3);
+	return 0;
+
+}
